636d17774499b36ec9da645ffe1ae170c712a1ed
Nhung Hoang <nhoang4673@gmail.com>
2020-12-26 23:05:33 -0600

    updated writings page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@
 	Hyperspace by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    
+    Modified by Nhung Hoang (2020) 
 */
 
 (function($) {
@@ -27,7 +29,7 @@
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
-			}, 100);
+			}, 0);
 		});
 
 	// Forms.
