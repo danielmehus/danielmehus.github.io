de20e75198dc0030cc083cae0279b9231433b7e5
danielmehus <76071523+danielmehus@users.noreply.github.com>
2025-09-06 16:54:32 -0500

    Create assets js theme.js

---
+++ assets js theme.js
@@ -0,0 +1,2 @@
+<script src="{{ '/assets/js/menu.js?v=' | append: site.github.build_revision | relative_url }}"></script>
+<script src="{{ '/assets/js/theme.js?v=' | append: site.github.build_revision | relative_url }}"></script>
